dac5b174557af859eb5cb81b9af1ff2b5408eaaa
scfung <90009041+scfung@users.noreply.github.com>
2023-11-17 20:14:21 -0500

    Update back-end.js

--- weather-app-backend/back-end.js
+++ weather-app-backend/back-end.js
@@ -1,4 +1,4 @@
-
+//Required modules for filing requests
 const cors = require('cors');
 const express = require('express');
 const axios = require('axios');
@@ -10,17 +10,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 app.use(cors());
-
+//Used to obtain information about the current weather conditions.
 app.get('/weather/current', async (req, res) => {
   try {
-    // Replace 'YOUR_API_KEY' with your actual API key from WeatherAPI.com
+    
     const apiKey = process.env.API_KEY;
-    const city = req.query.city || 'New York'; // You can change the default city as needed
+    const city = req.query.city || 'New York';
 
     // Make the API request
     const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${city}`);
 
-    // Handle the response data as needed
+    // Handle the response data 
 
     res.json(response.data);
   } catch (error) {
@@ -29,21 +29,23 @@ app.get('/weather/current', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
+//Used to obtain information about the forecast weather conditions.
 app.get('/weather/forecast', async (req, res) => {
     try {
-      const city = req.query.city || 'New York'; // You can change the default city as needed
+      const city = req.query.city || 'New York';
       const apiKey = process.env.API_KEY;
       // Make the API request for the hourly forecast
       const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${process.env.API_KEY}&q=${city}&days=4&aqi=no&alerts=no`);
-  
+      //Handles the response data
       res.json(response.data);
     } catch (error) {
+      //Handle errors
       console.error('Error fetching forecast:', error.message);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
 
+//Opens the port for frontend to access information.
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
